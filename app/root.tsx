95cbc2ef64e40c8ff98827ddca552bf947fcfff1
takara2314 <takara2314_TK@outlook.jp>
2024-03-03 03:45:18 +0900

    Include: Inter, M PLUS 2

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { cssBundleHref } from '@remix-run/css-bundle';
 import type { LinksFunction } from '@remix-run/node';
 import {
   Links,
@@ -8,12 +9,23 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 
+import '@fontsource-variable/inter/wght.css';
+import '@fontsource-variable/m-plus-2/wght.css';
+
 import stylesheet from '~/tailwind.css';
 import Menu from './components/menu/Menu';
 
-export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: stylesheet },
-];
+export const links: LinksFunction = () => {
+  return [
+    ...(cssBundleHref
+      ? [
+          { rel: 'stylesheet', href: cssBundleHref },
+          { rel: 'stylesheet', href: stylesheet },
+        ]
+      : []),
+    // ...
+  ];
+};
 
 export default function App() {
   return (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Inter Variable"',
+          '"M PLUS 2 Variable"',
+          'sans-serif',
+        ],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
