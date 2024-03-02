78f9923cdf4c6f7f31ba2d6e37fbd068ab784e77
takara2314 <takara2314_TK@outlook.jp>
2024-03-03 04:08:22 +0900

    Include: Three.js

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,11 @@
+import {
+  Canvas,
+  ThreeElements,
+  useFrame,
+} from '@react-three/fiber';
 import type { MetaFunction } from '@remix-run/node';
+import { useRef, useState } from 'react';
+import * as THREE from 'three';
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,6 +19,49 @@ export const meta: MetaFunction = () => {
 
 export default function Page() {
   return (
-    <main className="pl-4">たからーん（濱口 宝）</main>
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight
+        position={[-10, -10, -10]}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
+
+function Box(props: ThreeElements['mesh']) {
+  // biome-ignore lint/style/noNonNullAssertion: <explanation>
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial
+        color={hovered ? 'hotpink' : 'orange'}
+      />
+    </mesh>
   );
 }
