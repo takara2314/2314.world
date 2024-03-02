26664bf3e5fcd27e87e915ca4c4da55dad0a8afd
takara2314 <takara2314_TK@outlook.jp>
2024-03-03 03:36:56 +0900

    Feat: 実際の現在パスを取得するように

--- app/components/menu/PageSelectorItem.tsx
+++ app/components/menu/PageSelectorItem.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@remix-run/react';
+import { Link, useLocation } from '@remix-run/react';
 
 interface Props {
   label: string;
@@ -9,13 +9,13 @@ export default function PageSelectorItem({
   label,
   href,
 }: Props) {
-  const currentPath = '/';
+  const location = useLocation();
 
   return (
     <li>
       <Link to={href}>
         <div className="w-full h-10 relative">
-          {currentPath === href ? (
+          {location.pathname === href ? (
             <div className="w-11/12 h-full bg-lime-200 rounded-r-full absolute top-0 left-0" />
           ) : (
             <div className="w-11/12 h-full hover:bg-gray-200 rounded-r-full transition-colors duration-150 absolute top-0 left-0" />
@@ -23,7 +23,7 @@ export default function PageSelectorItem({
 
           <div
             className={
-              currentPath === href
+              location.pathname === href
                 ? 'w-full h-full text-lime-950 text-xl pointer-events-none flex flex-col items-center justify-center absolute top-0 left-0'
                 : 'w-full h-full text-xl pointer-events-none flex flex-col items-center justify-center absolute top-0 left-0'
             }
