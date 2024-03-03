6a1e641f05c9abd7c23f6708f627d748bd414505
takara2314 <takara2314_TK@outlook.jp>
2024-03-03 14:25:51 +0900

    Feat: スマホはメニューをスワイプして非表示にできるように

--- app/components/menu/Menu.tsx
+++ app/components/menu/Menu.tsx
@@ -1,11 +1,31 @@
+import { motion, useMotionValue } from 'framer-motion';
+import { useEffect } from 'react';
+import { useMediaQuery } from 'usehooks-ts';
 import Copyright from './Copyright';
 import Hero from './Hero';
 import PageSelector from './PageSelector';
 import SocialIcons from './SocialIcons';
 
 export default function Menu() {
+  const isPc = useMediaQuery('(min-width: 768px)');
+  const x = useMotionValue(0);
+
+  useEffect(() => {
+    if (!isPc) {
+      return;
+    }
+
+    x.set(0);
+  }, [isPc, x.set]);
+
   return (
-    <menu className="w-[20rem] h-full text-center bg-white rounded-r-2xl fixed top-0 left-0">
+    <motion.menu
+      drag={isPc ? undefined : 'x'}
+      dragConstraints={{ left: -500, right: 0 }}
+      dragElastic={0}
+      style={{ x }}
+      className="w-[20rem] h-full text-center bg-white rounded-r-2xl fixed top-0 left-0"
+    >
       <Hero />
       <PageSelector />
 
@@ -13,6 +33,6 @@ export default function Menu() {
         <SocialIcons />
         <Copyright />
       </div>
-    </menu>
+    </motion.menu>
   );
 }
